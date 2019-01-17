4d503f29c0edf190c39b2b083fc2357f97cd456a
Diego Gomes <diego.gbfernandes@gmail.com>
2019-01-16 22:05:37 -0200

    Validação customizada para CPF e CNPJ ao inserir novo Cliente

---
+++ src/main/java/br/com/projeto/meu/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,38 @@
+package br.com.projeto.meu.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import br.com.projeto.meu.domain.enums.TipoCliente;
+import br.com.projeto.meu.dto.ClienteNewDTO;
+import br.com.projeto.meu.resources.exception.FieldMessage;
+import br.com.projeto.meu.services.validation.util.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("CpfOuCnpj", "CPF Inválido"));
+		}
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("CpfOuCnpj", "CNPJ Inválido"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
